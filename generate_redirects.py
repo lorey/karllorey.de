8d538be9cefb868b5728723921998258ed0c234f
Karl Lorey <git@karllorey.com>
2020-02-28 14:38:21 +0100

    Add /tag/* redirects

--- generate_redirects.py
+++ generate_redirects.py
@@ -19,7 +19,7 @@ def generate_redirects(posts_directory):
     files_abs = [os.path.abspath(os.path.join(posts_directory, f)) for f in files]
 
     # generate redirect tuples (from, to)
-    redirects = []
+    redirects_posts = []
     for file_path in files_abs:
         with open(file_path, "r") as file:
             # I know this is ugly, but it gets the job done
@@ -29,12 +29,23 @@ def generate_redirects(posts_directory):
             url_path_old = urljoin(permalink, "index.html")
 
             # create the new path
+            # (doesn't need /index.html)
             base_path = "/{}".format(posts_directory)
             post_slug = Path(file_path).name.replace(".md", "")
-            url_path_new = "/".join([base_path, post_slug, "index.html"])
+            url_path_new = "/".join([base_path, post_slug])
 
-            redirects.append((url_path_old, url_path_new))
-    return redirects
+            redirects_posts.append((url_path_old, url_path_new))
+
+    with open("url_map.csv") as csv_file:
+        csv_lines = csv_file.readlines()
+    redirects_map_tuples = [l.split(",") for l in csv_lines]
+    redirects_map_tuples = [
+        (t[0].replace("http://www.karllorey.de", "") + "/index.html", t[1].strip())
+        for t in redirects_map_tuples
+    ]
+    redirects_tags = [t for t in redirects_map_tuples if t[0].startswith("/tag")]
+
+    return redirects_posts + redirects_tags
 
 
 def extract_permalink_from_meta(file):
